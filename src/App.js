bf41fc7ba241dca1ae569b18f89339eb7254ff0b
Ajaykumar03n <ajaykumarn.22aid@kongu.edu>
2024-10-20 10:48:43 +0530

    ajay finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginForm from './Components/LoginForm/LoginForm';
+import Main from './Components/Main/Main';
+import ServiceRecord from './Components/ServiceRecord/ServiceRecord';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/main/:bus_id" element={<Main />} /> {/* This expects a bus_id */}
+        <Route path="/summary" element={<ServiceRecord />} />
+      </Routes>
+
+    </Router>
   );
 }
 
